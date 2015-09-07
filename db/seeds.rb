e44010a13c98d98b91ed4cce167e1f044d0601fc
Di Wu <Di.Wu@sage.com>
2015-09-07 16:38:43 -0400

    add products and stype

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create!(
+    name: 'Peanut butter patties',
+    description: 'A layer of peanut butter with a rich chocolaty coating.',
+    image_url: 'pbp.jpg',
+    price: 3.50
+)
+Product.create!(
+    name: 'Caramel deLites',
+    description: 'caramel and toasted coconut-covered cookies.',
+    image_url: 'cd.jpg',
+    price: 3.50
+)
